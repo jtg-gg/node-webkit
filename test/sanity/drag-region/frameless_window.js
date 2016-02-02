8f918454f7cc35e05dca5ac338aaad40a34114b1
Jefry <jefry.ted@vcube.co.jp>
2016-02-02 17:48:20 +0700

    [Drag Region] all platform, window creation param, add automated test

---
+++ test/sanity/drag-region/frameless_window.js
@@ -0,0 +1,88 @@
+function updateCheckbox() {
+  var top_checkbox = document.getElementById("top-box");
+  var bottom_checkbox = document.getElementById("bottom-box");
+  var left_checkbox = document.getElementById("left-box");
+  var right_checkbox = document.getElementById("right-box");
+  if (top_checkbox.checked || bottom_checkbox.checked) {
+    left_checkbox.disabled = true;
+    right_checkbox.disabled = true;
+  } else if (left_checkbox.checked || right_checkbox.checked) {
+    top_checkbox.disabled = true;
+    bottom_checkbox.disabled = true;
+  } else {
+    left_checkbox.disabled = false;
+    right_checkbox.disabled = false;
+    top_checkbox.disabled = false;
+    bottom_checkbox.disabled = false;
+  }
+}
+
+function initCheckbox(checkboxId, titlebar_name, titlebar_icon_url, titlebar_text) {
+  var elem = document.getElementById(checkboxId);
+  if (!elem)
+    return;
+  elem.onclick = function() {
+    if (document.getElementById(checkboxId).checked)
+      addTitlebar(titlebar_name, titlebar_icon_url, titlebar_text);
+    else
+      removeTitlebar(titlebar_name);
+    focusTitlebars(true);
+
+    updateContentStyle();
+    updateCheckbox();
+  }
+}
+
+window.onfocus = function() { 
+  console.log("focus");
+  focusTitlebars(true);
+}
+
+window.onblur = function() { 
+  console.log("blur");
+  focusTitlebars(false);
+}
+
+window.onresize = function() {
+  updateContentStyle();
+}
+
+function writeSuccess(id) {
+  var content = document.getElementById("inner-content");
+  var result = document.createElement('p');
+  result.setAttribute('id', 'result' + id);
+  result.innerHTML = 'success '+ id;
+  content.appendChild(result);
+}
+
+function writeFailure(e, id) {
+  var content = document.getElementById("inner-content");
+  var result = document.createElement('p');
+  result.setAttribute('id', 'result' + id);
+  result.innerHTML = 'failure ' + id + ' [' + e + ']';
+  content.appendChild(result);
+}
+
+var winMetrics = [-1, -1, -1, -1];
+function getMetric() {
+  var window = nw.Window.get();
+  document.getElementById("output").innerHTML += "<br> window Metric: " + window.x + ", " + window.y + ", " + window.width + ", " + window.height;
+  winMetrics = [window.x, window.y, window.width, window.height];
+}
+
+window.onload = function() {
+  initCheckbox("top-box", "top-titlebar", "top-titlebar.png", "Top Titlebar");
+  initCheckbox("bottom-box", "bottom-titlebar", "bottom-titlebar.png", "Bottom Titlebar");
+  initCheckbox("left-box", "left-titlebar", "left-titlebar.png", "Left Titlebar");
+  initCheckbox("right-box", "right-titlebar", "right-titlebar.png", "Right Titlebar");
+  
+  document.getElementById("close-window-button").onclick = function() {
+    window.close();
+  }
+  
+  updateContentStyle();
+  
+  getMetric();
+  //setTimeout(getMetric, 3000)
+
+}
