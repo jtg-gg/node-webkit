04182dc3965a60f5e1a954e278f9127a0261d201
Jefry <jefry.ted@vcube.co.jp>
2017-01-10 18:54:57 +0700

    [WindowButtonsOffset][MacOS] nw2 implementation Squashed commits: [e3f69d24] [WindowButtonsOffset][OSX] HIDDEN_INSET titlebar fix for MacOS High Sierra [cde9785] [WindowButtonsOffset][OSX] add title_bar_style and javascript function to support this

--- src/resources/api_nw_window.js
+++ src/resources/api_nw_window.js
@@ -685,8 +685,24 @@ apiBridge.registerCustomHook(function(bindingsAPI) {
         options.icon = params.icon;
       if (params.id)
         options.id = params.id;
+      if (params.title_bar_style) {
+        options.title_bar_style = params.title_bar_style;
+        if (navigator.appVersion.indexOf("Mac")!=-1 &&
+            options.title_bar_style &&
+            options.title_bar_style.startsWith("hidden") &&
+            params.frame === false) {
+          options.frame = 'chrome';
+          options.force_enable_drag_region = true;
+        }
+      }
     }
     try_hidden(window).chrome.app.window.create(url, options, function(appWin) {
+      if (appWin &&
+        nw.require('os').platform() == "darwin" &&
+        options.title_bar_style &&
+        options.title_bar_style.startsWith("hidden-inset")) {
+        appWin.setWindowButtonsOffset();
+      }
       if (callback) {
         if (appWin)
           callback(try_nw(appWin.contentWindow).nw.Window.get());
